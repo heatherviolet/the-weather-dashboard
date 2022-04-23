d9189bce3ea3d8858b8bcdf5906361b2f6a5c7b0
Heather Violet <heatherviolet91@gmail.com>
2022-04-23 16:51:10 -0700

    request weather API and added CSS

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
-var response = fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}");
-console.log(response);
+var searchBtn = document.getElementById("searchBtn");
+var cityInput = document.getElementById("cityName");
+
+
+// request weather data from openweather API
+var getCity = function() {
+    var apiKey = "7b82dad76c0454a41390ac5b36cbf36e"
+    var cityName = cityInput.value
+    console.log("cityName", cityName);
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+
+searchBtn.addEventListener("click", getCity);
